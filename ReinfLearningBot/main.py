f336918bd2225c86ac46cd34a0ac58eba567267d
Alexandru Oană <oalexandruandrei@gmail.com>
2023-04-22 20:28:11 +0300

    Fix logs when running multiple environments.
    Rollout logs were missing since using the "SB3MultipleInstanceEnv" wrapper.

--- ReinfLearningBot/main.py
+++ ReinfLearningBot/main.py
@@ -13,14 +13,20 @@ from rlgym.envs import Match
 from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
 from rlgym_tools.extra_action_parsers.kbm_act import KBMAction
 
+from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecCheckNan
+
 from ObservationBuilder import CustomObsBuilder
 
 # ----------------
-# Variables
+# CONSTANTS
 DEFAULT_TICK_SKIP = 8
 PHYSICS_TICKS_PER_SECOND = 120
-EP_LEN_SECONDS = 15
+EP_LEN_SECONDS = 10
+GAMMA = 0.9
+TIME_STEPS = int(6e10)
 
+# ------------------
+# Variables
 max_steps = int(round(EP_LEN_SECONDS * PHYSICS_TICKS_PER_SECOND / DEFAULT_TICK_SKIP))
 
 liu_distance = LiuDistancePlayerToBallReward()
@@ -33,39 +39,31 @@ terminal_condition = TimeoutCondition(max_steps)
 def get_match():
     return Match(
         reward_function=liu_distance,
-        terminal_conditions=[terminal_condition],
+        terminal_conditions=[TimeoutCondition(225)],
         obs_builder=CustomObsBuilder(),
         state_setter=RandomState(),
         action_parser=KBMAction(),
+        tick_skip=DEFAULT_TICK_SKIP,
+
         # game_speed=1
     )
 
 
 if __name__ == "__main__":
-    # TODO: Change back to random state setter (remove if multiple instances work)
-    # env = rlgym.make(reward_fn=liu_distance,
-    #                  obs_builder=CustomObsBuilder(),
-    #                  terminal_conditions=[terminal_condition],
-    #                  game_speed=1
-    #                  )
-
-    env = SB3MultipleInstanceEnv(match_func_or_matches=get_match, num_instances=2, wait_time=20)
+    env = SB3MultipleInstanceEnv(match_func_or_matches=get_match, num_instances=8, wait_time=20)
+    env = VecMonitor(env)  # enables logging for rollout phase (episode length, mean reward)
+    env = VecNormalize(env, norm_obs=False, gamma=0.9)  # reward normalization
 
     # TODO: Try using a custom neural network architecture for training the policy
     model = PPO("MlpPolicy",
                 env=env,
-                n_steps=8192,
-                batch_size=256,
                 ent_coef=0.01,
-                verbose=1,
-                tensorboard_log="./rl_tensorboard_log")
-
-    # Increase the number of training steps
-    TIME_STEPS = int(6e10)
+                verbose=0,
+                tensorboard_log="./rl_tensorboard_log",
+                device="auto")
 
     # Use a checkpoint callback to save the model during training
-    checkpoint_callback = CheckpointCallback(save_freq=5000000, save_path='./logs/',
-                                             name_prefix='model')
+    checkpoint_callback = CheckpointCallback(save_freq=round(5_000_000 / env.num_envs), save_path='./logs/',
+                                             name_prefix="rl_model")
 
-    print("starts learning")
-    model.learn(total_timesteps=TIME_STEPS, callback=[checkpoint_callback])
+    model.learn(total_timesteps=TIME_STEPS, callback=[checkpoint_callback], tb_log_name="speed_objective")
